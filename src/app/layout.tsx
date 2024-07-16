fedca5929d6493440a6d2b49c94ebe9e06076ea1
srajan <kumarsrajan02@gmail.com>
2024-07-16 20:50:45 +0530

    update + metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,60 @@ import Background from "@/components/Background";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Srajan Kumar • Links",
+  title: {
+    default: "Srajan Kumar • Links",
+    template: "%s | Srajan's Website",
+  },
   description: "All my links in one place.",
+  keywords: [
+    "Srajan Kumar",
+    "Srajan Links",
+    "Srajan",
+    "Srajan Sahyadri",
+    "Srajan Kumar Sahyadri",
+    "Srajan Sahyadri College",
+    "Srajan Kumar Sahyadri College",
+    "Srajan SCEM",
+    "Srajan Kumar SCEM",
+    "Designer",
+    "Portfolio",
+    "Portfolio Website",
+    "Designer and Developer",
+    "Frontend Developer",
+    "Fullstack Developer",
+    "Full Stack Developer",
+    "React Developer",
+    "MERN Stack Developer",
+  ],
+  creator: "Srajan Kumar",
+  openGraph: {
+    title: "Srajan Kumar • Links",
+    description: "All my links in one place.",
+    url: "https://srajan.vercel.app/",
+    siteName: "Srajan Kumar • Links",
+    images: [
+      {
+        url: "https://srajan.vercel.app/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Srajan Kumar • Links",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Srajan Kumar • Links",
+    description: "All my links in one place.",
+    creator: "@kumarsrajan02",
+    images: ["https://srajan.vercel.app/og.png"],
+  },
+  icons: {
+    icon: "/favicons/favicon.ico",
+    shortcut: "/favicons/favicon-16x16.png",
+    apple: "/favicons/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -19,7 +71,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="relative mx-auto max-w-4xl px-5">
-          <Background />
+          {/* <Background /> */}
           <div className="flex justify-center items-center min-h-[100dvh]">
             {children}
           </div>
--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -14,12 +14,10 @@ import {
   GitHubLogoIcon,
   GlobeIcon,
 } from "@radix-ui/react-icons";
-import { ArrowRight, X } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 
 import Image from "next/image";
 import Link from "next/link";
-
-import data from "@/data.json";
 
 const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -42,7 +40,7 @@ const toBase64 = (str: string) =>
 
 export default function LinkCard() {
   return (
-    <Card className="flex tracking-wide flex-col w-[23rem] h-[41rem] scale-100 border-input border-[1px] border-tertiary bg-secondary/50">
+    <Card className="flex shadow-md tracking-wide flex-col w-[23rem] h-[41rem] scale-100 border-input border-[1px] border-tertiary bg-secondary/50">
       <CardHeader className="flex w-full justify-center items-center">
         <Image
           src="/srajan.jpg"
@@ -59,9 +57,7 @@ export default function LinkCard() {
         <CardTitle className="pt-5 tracking-wide pb-1 text-xl">
           Srajan Kumar
         </CardTitle>
-        <CardDescription>
-          CSE Undergrad • Full-Stack Web Developer
-        </CardDescription>
+        <CardDescription>CSE Undergrad • Full-Stack Developer</CardDescription>
       </CardHeader>
       <CardContent>
         <Tabs defaultValue="links" className="w-full flex flex-col">
@@ -71,64 +67,195 @@ export default function LinkCard() {
           </TabsList>
           <TabsContent value="links">
             <div className="flex flex-col py-1">
-              {data.links.map((link: any, index: number) => (
-                <Link key={index} href={link.url} target="_blank">
-                  <Button
-                    className="flex w-full justify-start py-9 rounded-xl gap-5"
-                    variant="ghost"
+              <Link href="https://srajan.tech" target="_blank">
+                <Button
+                  className="flex w-full justify-start py-9 rounded-xl gap-5"
+                  variant="ghost"
+                >
+                  <div
+                    className={`w-10 h-10 shadow-md bg-violet-500 bg-opacity-20 flex justify-center items-center rounded-xl`}
                   >
-                    <div
-                      className={`w-10 h-10 bg-${link.iconColor}-500 bg-opacity-20 flex justify-center items-center rounded-xl`}
-                    >
-                      {renderIcon(link.icon)}
+                    <GlobeIcon className="w-6 h-6 text-violet-500 will-change-transform will-change-opacity" />
+                  </div>
+                  <div className="text-start tracking-wide">
+                    <p>Website</p>
+                    <p className="text-xs font-light text-muted-foreground">
+                      srajan.tech
+                    </p>
+                  </div>
+                </Button>
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/kumarsrajan/"
+                target="_blank"
+              >
+                <Button
+                  className="flex w-full justify-start py-9 rounded-xl gap-5"
+                  variant="ghost"
+                >
+                  <div
+                    className={`w-10 h-10 shadow-md bg-blue-500 bg-opacity-20 flex justify-center items-center rounded-xl`}
+                  >
+                    <LinkedInLogoIcon className="w-6 h-6 text-blue-500 will-change-transform will-change-opacity" />
+                  </div>
+                  <div className="text-start tracking-wide">
+                    <p>LinkedIn</p>
+                    <p className="text-xs font-light text-muted-foreground">
+                      linkedin.com/in/kumarsrajan
+                    </p>
+                  </div>
+                </Button>
+              </Link>
+              <Link href="https://github.com/srajankumar/" target="_blank">
+                <Button
+                  className="flex w-full justify-start py-9 rounded-xl gap-5"
+                  variant="ghost"
+                >
+                  <div
+                    className={`w-10 h-10 shadow-md bg-gray-500 bg-opacity-20 flex justify-center items-center rounded-xl`}
+                  >
+                    <GitHubLogoIcon className="w-6 h-6 text-white will-change-transform will-change-opacity" />
+                  </div>
+                  <div className="text-start tracking-wide">
+                    <p>GitHub</p>
+                    <p className="text-xs font-light text-muted-foreground">
+                      github.com/srajankumar
+                    </p>
+                  </div>
+                </Button>
+              </Link>
+              <Link
+                href="https://www.instagram.com/withpainsimon/"
+                target="_blank"
+              >
+                <Button
+                  className="flex w-full justify-start py-9 rounded-xl gap-5"
+                  variant="ghost"
+                >
+                  <div
+                    className={`w-10 h-10 shadow-md bg-pink-500 bg-opacity-20 flex justify-center items-center rounded-xl`}
+                  >
+                    <InstagramLogoIcon className="w-6 h-6 text-pink-500 will-change-transform will-change-opacity" />
+                  </div>
+                  <div className="text-start tracking-wide">
+                    <p>Instagram</p>
+                    <p className="text-xs font-light text-muted-foreground">
+                      instagram.com/withpainsimon
+                    </p>
+                  </div>
+                </Button>
+              </Link>
+              <Link href="https://x.com/kumarsrajan02" target="_blank">
+                <Button
+                  className="flex w-full justify-start py-9 rounded-xl gap-5"
+                  variant="ghost"
+                >
+                  <div
+                    className={`w-10 h-10 shadow-md bg-gray-500 bg-opacity-20 flex justify-center items-center rounded-xl`}
+                  >
+                    <div className="w-6 flex justify-center items-center h-6 text-white will-change-transform will-change-opacity">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="22"
+                        height="22"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.215 2.25H8.04l4.713 6.231zm-1.161 17.52h1.833L7.045 4.126H5.078z"
+                        />
+                      </svg>
                     </div>
-                    <div className="text-start tracking-wide">
-                      <p>{link.name}</p>
-                      <p className="text-xs font-light text-muted-foreground">
-                        {link.description}
-                      </p>
-                    </div>
-                  </Button>
-                </Link>
-              ))}
+                  </div>
+                  <div className="text-start tracking-wide">
+                    <p>X</p>
+                    <p className="text-xs font-light text-muted-foreground">
+                      x.com/kumarsrajan02
+                    </p>
+                  </div>
+                </Button>
+              </Link>
             </div>
           </TabsContent>
           <TabsContent value="projects">
             <div className="flex flex-col py-1">
-              {data.projects.map((project: any, index: number) => (
-                <Link key={index} href={project.url} target="_blank">
-                  <Button
-                    className="flex w-full justify-start py-9 rounded-xl gap-5"
-                    variant="ghost"
-                  >
-                    {project.image && (
-                      <Image
-                        src={`/projects/${project.image}`}
-                        alt={project.name}
-                        placeholder={`data:image/svg+xml;base64,${toBase64(
-                          shimmer(700, 475)
-                        )}`}
-                        width={500}
-                        height={500}
-                        className="w-10 h-10 rounded-xl object-cover"
-                      />
-                    )}
-                    <div className="text-start tracking-wide">
-                      <p>{project.name}</p>
-                      <p className="text-xs font-light text-muted-foreground">
-                        {project.description}
-                      </p>
-                    </div>
-                  </Button>
-                </Link>
-              ))}
+              <Link href="https://quick-y.vercel.app" target="_blank">
+                <Button
+                  className="flex w-full justify-start py-9 rounded-xl gap-5"
+                  variant="ghost"
+                >
+                  <Image
+                    src="/projects/quick-y.png"
+                    alt="Quick-y"
+                    placeholder={`data:image/svg+xml;base64,${toBase64(
+                      shimmer(700, 475)
+                    )}`}
+                    width={500}
+                    height={500}
+                    className="w-10 h-10 shadow-md rounded-xl object-cover"
+                  />
+                  <div className="text-start tracking-wide">
+                    <p>Quick-y</p>
+                    <p className="text-xs font-light text-muted-foreground">
+                      Time-efficient healthcare.
+                    </p>
+                  </div>
+                </Button>
+              </Link>
+              <Link href="https://faceshare.vercel.app" target="_blank">
+                <Button
+                  className="flex w-full justify-start py-9 rounded-xl gap-5"
+                  variant="ghost"
+                >
+                  <Image
+                    src="/projects/faceshare.png"
+                    alt="Faceshare"
+                    placeholder={`data:image/svg+xml;base64,${toBase64(
+                      shimmer(700, 475)
+                    )}`}
+                    width={500}
+                    height={500}
+                    className="w-10 h-10 shadow-md rounded-xl object-cover"
+                  />
+                  <div className="text-start tracking-wide">
+                    <p>Faceshare</p>
+
+                    <p className="text-xs font-light text-muted-foreground">
+                      A Link in Bio. But Rich and Beautiful.
+                    </p>
+                  </div>
+                </Button>
+              </Link>
+              <Link href="https://susply.vercel.app" target="_blank">
+                <Button
+                  className="flex w-full justify-start py-9 rounded-xl gap-5"
+                  variant="ghost"
+                >
+                  <Image
+                    src="/projects/susply.png"
+                    alt="Susply"
+                    placeholder={`data:image/svg+xml;base64,${toBase64(
+                      shimmer(700, 475)
+                    )}`}
+                    width={500}
+                    height={500}
+                    className="w-10 h-10 shadow-md rounded-xl object-cover"
+                  />
+                  <div className="text-start tracking-wide">
+                    <p>Susply</p>
+                    <p className="text-xs font-light text-muted-foreground">
+                      Sustainable Supply Chain solution.
+                    </p>
+                  </div>
+                </Button>
+              </Link>
               <Link
                 href="https://www.srajan.tech/projects"
                 target="_blank"
                 className="flex justify-center items-center mt-5"
               >
-                <div className="no-underline group cursor-pointer relative shadow-2xl rounded-lg p-px text-xs font-semibold leading-6  text-white inline-block">
-                  <div className="relative text-sm font-medium tracking-wider flex space-x-2 items-center z-10 rounded-lg py-2 px-4 ring-1 ring-white/10 ">
+                <div className="no-underline group cursor-pointer relative shadow-2xl rounded-lg p-px text-xs font-semibold leading-6 text-white inline-block">
+                  <div className="relative text-xs font-medium tracking-wider flex space-x-2 items-center z-10 rounded-lg py-2 px-4 ring-1 ring-white/10">
                     <span>View all Projects</span>
                     <ArrowRight className="w-3 h-3" />
                   </div>
@@ -141,43 +268,4 @@ export default function LinkCard() {
       </CardContent>
     </Card>
   );
-}
-
-function renderIcon(icon: string) {
-  switch (icon) {
-    case "GlobeIcon":
-      return (
-        <GlobeIcon className="w-6 h-6 text-violet-500 will-change-transform will-change-opacity" />
-      );
-    case "LinkedInLogoIcon":
-      return (
-        <LinkedInLogoIcon className="w-6 h-6 text-blue-500 will-change-transform will-change-opacity" />
-      );
-    case "GitHubLogoIcon":
-      return (
-        <GitHubLogoIcon className="w-6 h-6 text-white-500 will-change-transform will-change-opacity" />
-      );
-    case "InstagramLogoIcon":
-      return (
-        <InstagramLogoIcon className="w-6 h-6 text-pink-500 will-change-transform will-change-opacity" />
-      );
-    case "XIcon":
-      return (
-        <div className="w-6 flex justify-center items-center h-6 text-white will-change-transform will-change-opacity">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="22"
-            height="22"
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentColor"
-              d="M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.215 2.25H8.04l4.713 6.231zm-1.161 17.52h1.833L7.045 4.126H5.078z"
-            />
-          </svg>
-        </div>
-      );
-    default:
-      return null;
-  }
 }
